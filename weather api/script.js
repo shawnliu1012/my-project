bc0b500d4b092b07c721c901e83165a38e722cea
liuyihsuan <liuyihsuan1012@gmail.com>
2021-12-06 15:29:20 +0800

    JS fetch api

--- weather api/script.js
+++ weather api/script.js
@@ -1,1 +1,24 @@
+fetch(
+    "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-566FD187-2A7C-4845-AC20-59E5816BA0BA&format=JSON&locationName=&elementName="
+)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (data) {
+        // console.log(data);
+        const weatherData = data.records.location;
 
+
+        [...weatherData].forEach((currentValue, index) => {
+            let name = weatherData[index].locationName;
+            let Rain =
+                weatherData[index].weatherElement[1].time[2].parameter.parameterName;
+            let Wx =
+                weatherData[index].weatherElement[0].time[2].parameter.parameterName;
+            let MinT =
+                weatherData[index].weatherElement[2].time[2].parameter.parameterName;
+            let MaxT =
+                weatherData[index].weatherElement[4].time[2].parameter.parameterName;
+            // console.log(currentValue);
+        });
+    });
